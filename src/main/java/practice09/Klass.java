b0973b0e49450f9f29414f782b157f768cc1e6c5
Bruno8310 <gongl3730@gmail.com>
2020-07-22 21:33:34 +0800

    Determine if a student joins the class

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,43 @@
 package practice09;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    private Student leader;
+
+    private int number;
+
+    public Klass() {
+
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == null) {
+            System.out.println("It is not one of us");
+        } else {
+        this.leader = student;
+        }
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+            student.setKlass(this);
+    }
 }
