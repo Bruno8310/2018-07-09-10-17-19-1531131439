18551b7701fdb5add9d537305b29f6753a55e9ed
Bruno8310 <gongl3730@gmail.com>
2020-07-22 22:53:50 +0800

    [practice11][Bruno] feature: Delivers message fo r student operations

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,43 @@
 package practice11;
 
+
+
 public class Klass {
+    private Student leader;
+
+    private int number;
+
+    public Klass() {
+
+    }
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == null) {
+            System.out.println("It is not one of us");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
+    public boolean isIn(Student student) {
+        return student.getKlass() == this;
+    }
 }
--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,30 @@
 package practice11;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.getName()+"."+ " I am " + this.getAge() + " years old.";
+    }
 }
