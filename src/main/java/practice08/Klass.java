9b6d4836f0cc237b27fb820650fbc3fe26d92b86
Bruno8310 <gongl3730@gmail.com>
2020-07-22 20:42:07 +0800

    [practice08][Bruno] feature: Compare the student is a leader or not

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
 public class Klass {
+    private Student leader;
+
+    private int number;
+
+    public Klass() {
+
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
 }
